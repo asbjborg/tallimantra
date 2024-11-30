b9d5c247ee9a260ebb8a1fa5ec8db18c97c67a02
AsbjBorg <contact@moosmann-consulting.com>
2024-11-30 22:49:57 +0100

    fix: batch fix test configuration and TypeScript errors

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,16 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/.next/'],
   transformIgnorePatterns: [
     'node_modules/(?!(electron|dotenv)/)',
   ],
   moduleDirectories: ['node_modules', 'src'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/.next/',
+    '\\.d\\.ts$'
+  ],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx']
 }; 
